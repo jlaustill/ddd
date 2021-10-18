140d144d1340248ce6f1d262c292cc6b2beaca0a
Joshua Austill <joshua.austill@livelyme.com>
2021-10-18 16:23:38 -0600

    setup prisma with mongodb, sweetness

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,38 @@
-const a: number = 3;
+import { PrismaClient } from '@prisma/client';
 
-// eslint-disable-next-line no-console
-console.log(a);
+const prisma = new PrismaClient();
+
+async function mainAsync() {
+  // Connect the client
+  await prisma.$connect();
+  // ... you will write your Prisma Client queries here
+  // await prisma.user.create({
+  //   data: {
+  //     name: 'Rich',
+  //     email: 'hello@prisma.com',
+  //     posts: {
+  //       create: {
+  //         title: 'My first post',
+  //         body: 'Lots of really interesting stuff',
+  //         slug: 'my-first-post',
+  //       },
+  //     },
+  //   },
+  // });
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
+  // eslint-disable-next-line no-console
+  console.dir(allUsers, { depth: null });
+}
+
+mainAsync()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
